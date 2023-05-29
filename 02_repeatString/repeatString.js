71127b833663cdd6842ab07301e97c953d780520
vjhwong <victor.ju.hin.wong@gmail.com>
2023-05-29 13:57:00 +0200

    Added code to repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  let repeatedString = string;
 
+  for (let index = 0; index < num - 1; index++) {
+    repeatedString += string;
+  }
+  return repeatedString;
 };
 
 // Do not edit below this line
