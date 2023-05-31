f082c505360b9bc32fc471b531f63dcbbdbf5967
vjhwong <victor.ju.hin.wong@gmail.com>
2023-05-31 18:32:43 +0200

    done with 2, 3, 4

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,7 +1,9 @@
 const repeatString = function (string, num) {
-  let repeatedString = string;
+  if (num < 0) return "ERROR";
 
-  for (let index = 0; index < num - 1; index++) {
+  let repeatedString = "";
+
+  for (let index = 0; index < num; index++) {
     repeatedString += string;
   }
   return repeatedString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...args) {
+  args.forEach((arg) => {
+    const index = array.indexOf(arg);
+    if (index > -1) {
+      array.splice(index, 1);
+    }
+  });
+  return array;
 };
 
 // Do not edit below this line
