eb6c7225e299a9e96e1b32b32f9253c80f434bdc
vjhwong <victor.ju.hin.wong@gmail.com>
2023-05-31 18:47:44 +0200

    finished assignment 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (
+    typeof num1 !== "number" ||
+    typeof num2 !== "number" ||
+    num1 < 0 ||
+    num2 < 0
+  )
+    return "ERROR";
 
+  const maxNumber = Math.max(num1, num2);
+  const minNumber = Math.min(num1, num2);
+
+  let sum = 0;
+  for (let index = minNumber; index < maxNumber + 1; index++) {
+    sum += index;
+  }
+  return sum;
 };
 
 // Do not edit below this line
